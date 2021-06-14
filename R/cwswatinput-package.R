370b45e76b9afd8b08d345f057b5967ed454c777
Reginal Exavier <reginalexavier@rocketmail.com>
2021-06-14 02:01:18 -0400

    add ts_to_area fucntion

---
+++ R/cwswatinput-package.R
@@ -0,0 +1,17 @@
+#' cwswatinput: Climate & Weater SWAT Input.
+#'
+#' Climate & Weater SWAT input (cwswatinput) is an open-source R package for
+#' preparing weather and climate data for the Soil & Water Assessment Tool (SWAT)
+#' input format.
+#'
+#'
+#'
+#' @author Reginal Exavier \email{reginalexavier@@rocketmail.com}, Peter Zeilhofer \email{zeilhoferpeter@@gmail.com}
+#' @docType package
+#' @name cwswatinput-package
+#' @aliases cwswatinput
+#'
+#'
+#'
+#'
+NULL
--- R/point_interpolation.R
+++ R/point_interpolation.R
@@ -1,6 +1,6 @@
 
 
-#' Trend Surface Interpolation by Targeded Points
+#' Trend Surface Interpolation into Targeded Points
 #'
 #' This function make an interpolation whith the trend surface method where the user
 #' have to inform the polynome degree. The interpolation is made over the tageded points
@@ -17,11 +17,11 @@
 #' @importFrom stats as.formula lm predict
 #'
 #' @examples
-#' ts_bypoint(my_folder = system.file("extdata/ts_input", package = "cwswatinput"),
+#' ts_to_point(my_folder = system.file("extdata/ts_input", package = "cwswatinput"),
 #'           targeted_points_path = system.file("extdata/sl_centroides/Centroide_watershed_grau.shp",
 #'            package = "cwswatinput"),
 #'           poly_degree = 2)
-ts_bypoint <- function(my_folder,
+ts_to_point <- function(my_folder,
                        targeted_points_path,
                        poly_degree = 2){
 
@@ -89,6 +89,98 @@ ts_bypoint <- function(my_folder,
 
 }
 
+
+#' Trend Surface Interpolation into Raster
+#'
+#' This function make an interpolation whith the trend surface method where the user
+#' have to inform the polynome degree. The interpolation is made over the tageded points
+#' for all the serie on the input.
+#'
+#' @param my_folder Folder containing the ts input files.
+#' @param bassin_limit_path A shapefile containing the bassin limit where the trend
+#' suface function have to predict values.
+#' @param poly_degree The degree to be used in the polynomial function for the trend surface.
+#' @param resolution The resolution for the output raster in degree.
+#'
+#' @return A rasterbrick
+#' @export
+#'
+#' @examples
+#' ts_to_area(my_folder = system.file("extdata/ts_input", package = "cwswatinput"),
+#'           bassin_limit_path = system.file("extdata/sl_centroides/Centroide_watershed_grau.shp",
+#'           package = "cwswatinput"),
+#'           poly_degree = 2,
+#'           resolution = 0.5)
+ts_to_area <- function(my_folder,
+                      bassin_limit_path,
+                      poly_degree = 2,
+                      resolution = 0.01){
+
+  bassin_limit <- sf::read_sf(bassin_limit_path)
+
+  var_files <- list.files(my_folder,
+                          full.names = TRUE,
+                          pattern = ".csv$")
+
+
+  # temp_name <- list.files(my_folder,
+  #                         full.names = FALSE)
+
+  names_sans_ext <- tools::file_path_sans_ext(list.files(my_folder,
+                                                         full.names = FALSE))
+
+
+  raster_list <- vector(mode = "list", length = length(var_files)) # blank list for future alocation
+  non_zero <- function(x){ifelse(x < 0, 0, x)}
+
+  # template grid
+  bbox <- c(
+    "xmin" = sf::st_bbox(bassin_limit)[[1]],
+    "ymin" = sf::st_bbox(bassin_limit)[[2]],
+    "xmax" = sf::st_bbox(bassin_limit)[[3]],
+    "ymax" = sf::st_bbox(bassin_limit)[[4]]
+  )
+
+  grd_template_sl <- expand.grid(
+    LONG = seq(from = bbox["xmin"], to = bbox["xmax"], by = resolution),
+    LAT = seq(from = bbox["ymin"], to = bbox["ymax"], by = resolution)
+  )
+
+  # iteração com progress bar
+  pb <- txtProgressBar(min = 0, max = length(raster_list), style = 3)
+  for (i in seq_along(var_files)) {
+
+    pcp_temp <- vroom::vroom((var_files[i]),
+                             delim = ",",
+                             col_types = "dcdddd")
+
+    # o polynomio do modelo
+    my_formula <- as.formula(pcp ~ poly(LONG, LAT, degree = poly_degree))
+
+    # ajuste do modelo
+    fit_lm <- lm(my_formula, data = pcp_temp)
+
+    # estimando para os grids
+    interpolation <- dplyr::mutate(grd_template_sl,
+                                   Z = non_zero(predict(fit_lm,
+                                                        grd_template_sl)))
+    point_2_raster <- raster::rasterFromXYZ(interpolation,
+                                            crs = "+proj=longlat +datum=WGS84 +no_defs")
+
+    raster_list[[i]] <- point_2_raster
+
+    setTxtProgressBar(pb, i)
+  }
+
+  close(pb) # fim do bloco
+
+  # renaming the objects list by the date
+  names(raster_list) <- names_sans_ext
+
+  # creating a raster brick
+  raster::brick(raster_list)
+
+}
 
 
 #' Main table creator for SWAT Input from Trend SUrface Interpolation
